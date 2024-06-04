9ac09e4d300a98f3b88fc5543357e844ddd26507
Leon Seemann <79045490+leonseemann@users.noreply.github.com>
2024-06-04 08:43:38 +0200

    Initial commit

---
+++ src/main/java/de/rbbk/databasefilebenchmark/DatabaseFileBenchmarkApplication.java
@@ -0,0 +1,13 @@
+package de.rbbk.databasefilebenchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseFileBenchmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseFileBenchmarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/rbbk/databasefilebenchmark/DatabaseFileBenchmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rbbk.databasefilebenchmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseFileBenchmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
